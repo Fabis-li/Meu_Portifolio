44384635793fa5cbe7358d100f1a0d32d2b5b4ad
Fabis-li <fabiano_slima@hotmail.com>
2024-10-08 11:15:04 -0300

    criado pagina de portifolio

---
+++ src/app/components/secoes/portifolio/portifolio.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portifolio',
+  standalone: true,
+  imports: [],
+  templateUrl:"./portifolio.component.html",
+  animations: [
+    
+  ],
+  
+})
+export class PortifolioComponent {
+
+}
