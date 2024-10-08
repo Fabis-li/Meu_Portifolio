86eb9c1809baf735546edece20f2e11e49e245d7
Fabis-li <fabiano_slima@hotmail.com>
2024-10-08 11:12:11 -0300

    implementado componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { SobreComponent } from "./components/secoes/sobre/sobre.component";
+import { HabilidadesComponent } from "./components/secoes/habilidades/habilidades.component";
+import { StackComponent } from './components/secoes/stack/stack.component';
+import { ExperienciaComponent } from "./components/secoes/experiencia/experiencia.component";
+import { PortifolioComponent } from "./components/secoes/portifolio/portifolio.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  imports: [NavbarComponent, FooterComponent, SobreComponent, HabilidadesComponent, StackComponent, ExperienciaComponent, PortifolioComponent],  
+  templateUrl: "app.component.html",
+  styleUrl: "app.component.scss",
+ 
 })
 export class AppComponent {
   title = 'portifolio';
