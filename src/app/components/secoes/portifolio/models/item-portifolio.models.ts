2e90241eafd1d562436d357891544fd3b34e6dfd
Fabis-li <fabiano_slima@hotmail.com>
2024-11-13 07:37:29 -0300

    implementado carousel

---
+++ src/app/components/secoes/portifolio/models/item-portifolio.models.ts
@@ -0,0 +1,5 @@
+export interface itemPortifolio {
+    href: string;
+    imgUrl: string;
+    descricao: string;
+}
--- src/app/components/secoes/portifolio/portifolio.component.ts
+++ src/app/components/secoes/portifolio/portifolio.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { itemPortifolio } from "./models/item-portifolio.models";
+import { NgClass, NgForOf } from "@angular/common";
 
 @Component({
-  selector: 'app-portifolio',
+  selector: "app-portifolio",
   standalone: true,
-  imports: [],
-  templateUrl:"./portifolio.component.html",
-  animations: [
-    
-  ],
-  
+  imports: [NgForOf, NgClass],
+  templateUrl: "./portifolio.component.html",
+  animations: [],
 })
-export class PortifolioComponent {
-
+export class PortifolioComponent {  
+  public itensPortifolio: itemPortifolio[] = [
+    {
+      href: "#",
+      imgUrl: "./assets/arm_peixe.png",
+      descricao: "Armadura de Peixes",
+    },
+    {
+      href: "#",
+      imgUrl: "./assets/arm_virgem.jpg",
+      descricao: "Armadura de Virgem",
+    },
+    {
+      href: "#",
+      imgUrl: "./assets/pegasus.jpg",
+      descricao: "Caveleiro de Pegasus",
+    },    
+  ];
 }
